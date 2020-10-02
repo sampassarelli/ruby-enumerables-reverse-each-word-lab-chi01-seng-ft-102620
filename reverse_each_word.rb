a53e6a6099fcd56d4cd5f7afe15015a79084274e
Sam Passarelli <passarelli.sam@gmail.com>
2020-10-02 02:51:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 def reverse_each_word(string)
-  string_array = string.split(" ")
+  string_array = string.split
   new_array = []
   string_array.each do |element|
     new_array << element.reverse
   end
-  new_array.join(" ")
+  new_array.join
 end
 
 def reverse_each_word(string)
