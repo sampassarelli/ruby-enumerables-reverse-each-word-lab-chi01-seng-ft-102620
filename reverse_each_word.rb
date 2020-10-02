aefab310f379c73124853e4c0ca311f422730903
Sam Passarelli <passarelli.sam@gmail.com>
2020-10-02 02:54:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(string)
   string_array.each do |element|
     new_array << element.reverse
   end
-  new_array.join
+  new_array.join(" ")
 end
 
 def reverse_each_word(string)
